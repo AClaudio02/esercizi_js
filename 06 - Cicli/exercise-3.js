406b8e64d08a52cb476c4dd29b997466361d725c
Claudio Atzori <claudio.atzori02@gmail.com>
2022-01-16 12:05:37 +0100

    Esercizio 6 completato

---
+++ 06 - Cicli/exercise-3.js
@@ -0,0 +1,28 @@
+function adultFilter(persons) {
+  let arrayEmpty = [];
+    for (let x = 0; x < persons.length; x++) {
+      
+      if (persons[x].age >= 18) {
+        arrayEmpty.push(persons[x]);
+        
+      }
+    }
+  return arrayEmpty;
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const adults = adultFilter(persons);
+console.log(persons);
+console.log(adults);
