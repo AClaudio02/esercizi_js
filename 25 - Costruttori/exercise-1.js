30b25639faed61a20c04ef885ab22d7cfe02d190
Claudio Atzori <claudio.atzori02@gmail.com>
2022-01-19 17:58:05 +0100

    Esercizio 26 completato

--- 25 - Costruttori/exercise-1.js
+++ 25 - Costruttori/exercise-1.js
@@ -1,12 +1,30 @@
-function Person(firstName, lastName){
-  this.firstName = firstName ;
-  this.lastName = lastName ;
-  this.fullName = () => {
-    return `${firstName} ${lastName}`
+const person = {
+  get firstName() {
+    return this.name;
+  },
+  set firstName(firstName) {
+    return this.name = firstName
+  } ,
+  get lastName() {
+    return this.surname;
+  },
+  set lastName(lastName) {
+    return this.surname = lastName
+  },
+  fullName() {
+    return `${this.firstName} ${this.lastName}`;
   }
+
 }
-let john = new Person("John", "Doe")
-let simon = new Person("Simon", "Collins");
+//  Oggetto "john"
+const john = Object.create(person)
+  john.firstName = "John"
+  john.lastName = "Doe"
+// Oggetto "simon"
+const simon = Object.create(person)
+  simon.firstName = "Simon"
+  simon.lastName = "Collins"
+
 
 console.log(john.fullName()); // John Doe
 console.log(simon.fullName()); // Simon Collins
