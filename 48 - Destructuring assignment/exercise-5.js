87f2224aa9e94d032dd1962db9a8fab1bebdf185
Claudio Atzori <claudio.atzori02@gmail.com>
2022-02-25 12:21:40 +0100

    Esercizio 48 fix

--- 48 - Destructuring assignment/exercise-5.js
+++ 48 - Destructuring assignment/exercise-5.js
@@ -5,10 +5,5 @@ const person = {
   age: 25,
 };
 
-const {
-  id,
-  name = person.firstName,
-  surname = person.lastName,
-  old = person.age,
-} = person;
+const { id, firstName: name, lastName: surname, age: old } = person;
 console.log(id, name, surname, old);
